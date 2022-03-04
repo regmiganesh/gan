659185054c84bf8a56bde824988aaa73f5426564
Ganesh Regmi <98951707+regmiganesh@users.noreply.github.com>
2022-03-04 15:33:13 +0545

    Add files via upload

---
+++ app.js/app.js
@@ -0,0 +1,22 @@
+
+/*js for navbar*/
+function openNav(){
+    document.getElementById('mySidebar').style.width = "40vh";
+    
+   
+   
+ }
+ function closeNav(){
+     document.getElementById('mySidebar').style.width = "0vh";
+ }
+
+
+
+ /* js  for social media */
+  
+function myfunction(){
+    window.scrollTo(0,0);
+}
+
+
+
